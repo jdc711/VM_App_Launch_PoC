4fb669426387f0ef9e9982d53d072cb0dfcc3a54
Joshua Choe <choejd@usc.edu>
2022-07-26 22:39:28 -0700

    add javascript to detect installed app from web browser... works for edge windows 10 and google chrome

--- WinApp/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ WinApp/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -61,7 +61,7 @@ namespace WinApp {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WinApp;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WinApp;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
